aec235c700edb178b32a2d168716260c65cf645a
Ali Maher <ali.maher.mohammed@gmail.com>
2021-02-02 05:39:15 +0200

    My first react native project

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  SafeAreaView,
+  Alert,
+  Button,
+  StatusBar,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  let x = 1;
+  console.log("It works");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello from React Native app!</Text>
+      <TouchableHighlight onPress={() => console.log("Icon Pressed!")}>
+        <Image
+          source={require("./assets/icon.png")}
+          style={{ width: 100, height: 100 }}
+        />
+      </TouchableHighlight>
+      <Button
+        title="Click Me!"
+        onPress={() => Alert.alert("Terms", "Do you agree?", [{ text: "yes" }])}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eee",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
